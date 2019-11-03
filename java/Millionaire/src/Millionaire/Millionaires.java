f9ff1cecddf81ce66484ff42493342e940248318
JessePha <57172556+JessePha@users.noreply.github.com>
2019-11-03 20:16:15 +0100

    New basic structure in Java

---
+++ java/Millionaire/src/Millionaire/Millionaires.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class MillionairesModel {
+    public static void main(String[] args){
+        new GameCenter();
+    }
+}
---
+++ java/Millionaire/src/Millionaire/Question.java
@@ -0,0 +1,24 @@
+public class Question{
+    String [] easyQuestionHtml = new String[5];
+    String [] mediumQuestionHtml = new String[5];
+    String [] hardQuestionHtml = new String[5];
+    String [] easyQuestionCss = new String[5];
+    String [] mediumQuestionCss = new String[5];
+    String [] hardQuestionCss = new String[5];
+    String [] easyQuestionJava = new String[5];
+    String [] mediumQuestionJava = new String[5];
+    String [] hardQuestionJava = new String[5];
+    String [] easyQuestionJavaScript = new String[5];
+    String [] mediumQuestionJavaScript = new String[5];
+    String [] hardQuestionJavaScript = new String[5];
+
+    String[] answer = new String[4];
+
+
+    public Question(){}
+
+    public Question(String string1, String string2, String string3, String string4,String string5) {
+
+
+    }
+}
