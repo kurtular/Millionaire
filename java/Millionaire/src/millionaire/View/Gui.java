695d09fe364b25c921278efce7328fe0d7672bf0
JoakimDenlov <57172462+JoakimDenlov@users.noreply.github.com>
2019-12-11 23:09:48 +0100

    Added reset method for LifeLineArea and LifeLine class.

--- java/Millionaire/src/millionaire/View/Gui.java
+++ java/Millionaire/src/millionaire/View/Gui.java
@@ -85,5 +85,6 @@ public class Gui extends Application {
     public static void reset(){
        PriceTable.getInstance().resetPriceTable();
        LifeLineArea.getInstance().resetLifeLineArea();
+       QuestionArea.getInstance().resetQuestionArea();
     }
 }
