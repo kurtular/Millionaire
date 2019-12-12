a2b3b66a36abe40d24b98940ad97eb39952739c3
93milaza <Github2019@hotmail.com>
2019-12-12 17:25:59 +0100

    Created a SetSecondsColor() Method in TimerLabel Class

--- java/Millionaire/src/millionaire/View/TimerLabel.java
+++ java/Millionaire/src/millionaire/View/TimerLabel.java
@@ -4,7 +4,6 @@ import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import millionaire.FINAL_GLOBAL_VARIABLES;
@@ -12,17 +11,15 @@ import millionaire.Timer;
 
 public class TimerLabel extends Label {
     private static final TimerLabel instance = new TimerLabel();
-
-    static TimerLabel getInstance() {
+    static TimerLabel getInstance(){
         return instance;
     }
 
     private boolean isStopped;
     private byte shownSeconds;
-
-    private TimerLabel() {
+    private TimerLabel(){
         super();
-        Circle shape = new Circle();
+        Circle shape =new Circle();
         shape.setRadius(110 / 2f);
         shape.setCenterX(110 / 2f);
         shape.setCenterY(110 / 2f);
@@ -33,45 +30,29 @@ public class TimerLabel extends Label {
         this.setGraphic(img);
         this.setWrapText(true);
         this.setTextFill(Color.valueOf("#fff"));
-        this.setFont(Font.font("arial", 20));
+        this.setFont(Font.font("arial",20));
         this.setContentDisplay(ContentDisplay.CENTER);
         this.setFocusTraversable(false);
     }
-
-    void startTimer() {
-        isStopped = false;
+    void startTimer(){
+        isStopped=false;
         setText(Integer.toString(shownSeconds));
-        setSecondsColor();
-        Timer.delay(() -> {
-            if (shownSeconds > 0 && !isStopped) {
+        Timer.delay(()->{
+            if(shownSeconds > 0 && !isStopped){
                 shownSeconds--;
                 startTimer();
             }
         }, 1);
     }
-
-    void resetTimer() {
+    void resetTimer(){
         stopTimer();
-        shownSeconds = (byte) FINAL_GLOBAL_VARIABLES.getQuestionDuration();
+        shownSeconds = (byte)FINAL_GLOBAL_VARIABLES.getQuestionDuration();
     }
-
-    void stopTimer() {
+    void stopTimer(){
         isStopped = true;
     }
-
     public byte getShownSeconds() {
         return shownSeconds;
     }
-
-    private void setSecondsColor() {
-        if (shownSeconds < 6) {
-            setTextFill(Paint.valueOf("#ff3434"));
-        } else if (shownSeconds < 16) {
-            setTextFill(Paint.valueOf("#faff6a"));
-        } else {
-            setTextFill(Paint.valueOf("#fff"));
-        }
-    }
 }
-
 
